741cc472d5bc1edb64dd4fa0f77e6000074d6e82
Feliverse <haybar@gmail.com>
2023-05-02 16:44:37 -0400

    fix rubocop ofensses

--- classes/game.rb
+++ classes/game.rb
@@ -2,16 +2,13 @@ require_relative 'item'
 class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
-  def initialize(public_date, multiplayer, last_played_at, genre, label, author)
+  def initialize(public_date, multiplayer, last_played_at)
     super(public_date)
     @multiplayer = multiplayer
-    @last_played_at = last_played_at  
+    @last_played_at = Date.parse(last_played_at)
   end
 
   def can_be_archive?
-    super && (Date.today - Date.parse(@last_played_at)) <= 730
+    super && (Date.today - @last_played_at) <= 730
   end
 end
-
-prueba = Game.new("2003/02/01", true, "2023/01/01", "terror", "Resident", "blizard")
-print prueba.can_be_archive?
