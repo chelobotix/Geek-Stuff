c0637d60e2603de9b3adfc307fd5bb62cf8290c0
Marcelo Alarcón <marceloalarconbarrenechea@gmail.com>
2023-05-03 16:55:35 -0400

    Update schema.sql file

--- models/db_schema.sql
+++ models/db_schema.sql
@@ -1,12 +1,9 @@
 CREATE DATABASE geek_stuff;
 
-CREATE TABLE music_albums(
-    id SERIAL PRIMARY KEY,
-    genre_id INT NOT NULL,
-    publish_date DATE NOT NULL,
-    on_spotify BOOLEAN FALSE,
-    archived BOOLEAN FALSE
-    CONSTRAINT fk_genre FOREIGN KEY (genre_id) REFERENCES genres(id)
+CREATE TABLE label(
+  id INT PRIMARY KEY,
+  title VARCHAR(250),
+  color VARCHAR(250)
 );
 
 CREATE TABLE genres(
@@ -14,12 +11,31 @@ CREATE TABLE genres(
   name VARCHAR(200),
 );
 
-CREATE TABLE game(
+CREATE TABLE author(
   id INT PRIMARY KEY,
-  publish_date DATE,
-  archived BOOLEAN,
-  multiplayer BOOLEAN,
-  last_paleyd_at DATE,
+  first_name VARCHAR(250),
+  last_name VARCHAR(250)
+);
+
+CREATE TABLE music(
+    id SERIAL PRIMARY KEY,
+    publish_date DATE NOT NULL,
+    archived BOOLEAN FALSE
+    on_spotify BOOLEAN FALSE,
+    label_id INT,
+    genre_id INT,
+    author_id INT,
+    CONSTRAINT fk_labels FOREIGN KEY(label_id) REFERENCES labels(id),
+    CONSTRAINT fk_authors FOREIGN KEY(author_id) REFERENCES author(id),
+    CONSTRAINT fk_genres FOREIGN KEY(genre_id) REFERENCES genres(id)
+);
+
+CREATE TABLE game(
+  id SERIAL PRIMARY KEY,
+  publish_date DATE NOT NULL,
+  archived BOOLEAN FALSE,
+  multiplayer BOOLEAN NOT NULL,
+  last_played_at DATE NOT NULL,
   label_id INT,
   genre_id INT,
   author_id INT,
@@ -28,8 +44,19 @@ CREATE TABLE game(
   CONSTRAINT fk_genres FOREIGN KEY(genre_id) REFERENCES genres(id)
 );
 
-CREATE TABLE author(
-  id INT PRIMARY KEY,
-  first_name VARCHAR(250),
-  last_name VARCHAR(250)
+CREATE TABLE book(
+  id SERIAL PRIMARY KEY,
+  publish_date DATE NOT NULL,
+  archived BOOLEAN FALSE,
+  cover_state VARCHAR(50) NOT NULL,
+  publisher VARCHAR(250) NOT NULL,
+  label_id INT,
+  genre_id INT,
+  author_id INT,
+  CONSTRAINT fk_labels FOREIGN KEY(label_id) REFERENCES labels(id),
+  CONSTRAINT fk_authors FOREIGN KEY(author_id) REFERENCES author(id),
+  CONSTRAINT fk_genres FOREIGN KEY(genre_id) REFERENCES genres(id)
 );
+
+
+
